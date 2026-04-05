1b156e2c69576d57027a96354dc28493c0178b26
Cenaida Huertas Rojas <chuertas21@unisalle.edu.co>
2026-04-05 02:02:42 +0000

    confirmo commit en estadopedido

---
+++ EstadoPedido.js
@@ -0,0 +1,26 @@
+
+    /**
+     * EstadoPedido - Enum de estados del pedido
+     */
+
+    const EstadoPedido = Object.freeze({
+        PENDIENTE: "PENDIENTE",
+        CONFIRMADO: "CONFIRMADO",
+        ENVIADO: "ENVIADO",
+        ENTREGADO: "ENTREGADO"
+        CANCELADO: "CANCELADO"
+        DESPACHADO: "DESPACHADO"
+    });
+
+    /**
+    * EstadoEnvio - Enum de estados del envío
+    */
+
+    const EstadoEnvio = Object.freeze({
+        PENDIENTE: "PENDIENTE"
+        EN_RUTA: "EN_RUTA"
+        ENTREGADO: "ENTREGADO"
+        DEVUELTO: "DEVUELTO"
+    });
+
+    modelu.exports= {EstadoPedido, EstadoEnvio};
